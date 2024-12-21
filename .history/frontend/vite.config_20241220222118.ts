3db6e2bf05bd11f1e841e2a39df8897257d90daf
KooshaPari <kooshapari@gmail.com>
2024-12-20 22:22:06 -0800

    odin

--- .history/frontend/vite.config_20241220222118.ts
+++ .history/frontend/vite.config_20241220222118.ts
@@ -2,9 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
-	 preview: {
-    host: '0.0.0.0',
-    port: 8080,
-  },
+	plugins: [sveltekit()sds
 });
